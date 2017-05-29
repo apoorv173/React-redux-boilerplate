feea58d029d89aa5b4df6ed97ce87bec7f837c3b
Apoorv Mittal <amittal15@sapient.com>
2017-05-29 14:06:54 +0100

    changes pushed with linting enabled

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,10 +10,10 @@ const App = () => (
   <main>
     <Header />
 
-    <Switch>
-      <Route path="/budget" component={Budget} />
-      <Route path="/reports" component={Reports} />
-      <Redirect to="/budget" />
+    			<Switch>
+          <Route path="/budget" component={Budget} />
+             <Route path="/reports" component={Reports} />
+      				<Redirect to="/budget" />
     </Switch>
   </main>
 );
